eef687d24eb7747f1cae6cad60cf4e3022cb6156
Darren Case <darren@darren-cases-macbook.local>
2009-02-18 12:00:51 -0500

    changed recipient email for contact form to client

--- app/models/contact_mailer.rb
+++ app/models/contact_mailer.rb
@@ -2,8 +2,8 @@ class ContactMailer < ActionMailer::Base
   
   def contact_form(contact)
     subject    'contact from sarahandersondesign.com'
-    recipients 'case_darren@yahoo.com'
-    from       'sarahandersondesign.com <sarahandersondesign.com>'
+    recipients 'info@sarahandersondesign.com'
+    from       'sarahandersondesign.com <info@sarahandersondesign.com>'
     sent_on    Time.now
     
     body       :contact => contact
