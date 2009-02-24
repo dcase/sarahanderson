a4e507c1f0989d06e9a9650d4e73115b311673ac
Darren Case <darren@darren-cases-macbook.local>
2009-02-24 15:32:47 -0500

    fixing home page error

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -6,4 +6,10 @@ class Piece < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :client_name
   validates_attachment_presence :image
+  
+  def after_destroy
+    if category.name == 'Home'
+      session[:home_image] = 0
+    end
+  end
 end
