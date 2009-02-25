2a49d8224c9bec0e61732dfc5c882d84e4173774
Darren Case <darren@darren-cases-macbook.local>
2009-02-25 17:19:06 -0500

    design tweaks

--- app/views/contacts/new.html.rb
+++ app/views/contacts/new.html.rb
@@ -2,7 +2,8 @@
 <div id="contact-wrapper">
   <div id="contact_info_container">
     <%= image_tag 'contact-me.gif', :size => '182x21', :alt => 'Contact me' %><br />
-    <%= link_to image_tag('contact-info.gif', :size => '183x28', :mouseover => 'contact-info_ro.gif'), {}, :href => 'mailto:info@sarahandersondesign.com' %>
+    <%= link_to image_tag('contact-email.gif', :size => '183x14', :mouseover => 'contact-email_ro.gif'), {}, :href => 'mailto:info@sarahandersondesign.com' %>
+    <%= image_tag 'contact-phone.gif', :size => '74x10', :alt => '(347) 276-6211' %>
     <div id="errors_container"> 
       <%= render :partial => 'error_messages', :object => @contact %>
     </div>
@@ -10,9 +11,6 @@
   <div id="contact_form_container">
     <%= render :partial => 'form', :object => @contact %>
   </div>
-</div>
-<div id="home-footer">
-  <%= image_tag 'art-direction-and-design.gif', :size => '118x8', :alt => 'Art Direction and Design' %>
 </div>
 
 <% content_for :page_js do %>
