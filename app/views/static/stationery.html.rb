c5af081a5fc9a08a0967744fa3eba066a6001f8a
Darren Case <darren@darren-cases-macbook.local>
2009-02-26 11:43:57 -0500

    tweaks round 2

--- app/views/static/stationery.html.rb
+++ app/views/static/stationery.html.rb
@@ -6,6 +6,6 @@
   <%= image_tag 'stationery-and-custom-invites.jpg', :size => '760x448', :alt => 'Stationery and Custome Invites: Coming soon' %>
 </div>
 <div id="stationery-footer">
-  <%= image_tag 'by-appointment-only.gif', :size => '210x10', :alt => 'By appointment only 347-276-6211' %>
+  <%= image_tag 'by-appointment-only.gif', :size => '208x10', :alt => 'By appointment only 347-276-6211' %>
   <%= link_to image_tag('email-address.gif', :size => '197x13', :alt => 'info at sarah anderson design dot com', :mouseover => 'email-address_ro.gif'), {}, :href => "mailto:info@sarahandersondesign.com" %>
 </div>
